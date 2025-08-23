39dace03a9a13ce1e059d0ad677c59c3a4703864
Tyreece Rozycki <57762380+KrystilizeNevaDies@users.noreply.github.com>
2025-08-24 01:21:22 +1000

    remove mist

--- Technolize/World/Block/Blocks.cs
+++ Technolize/World/Block/Blocks.cs
@@ -9,7 +9,6 @@ public static class Blocks
 
     public static BlockInfo Air { get; } = new (nameof(Air), NextId++, new (1, 1), MatterState.Gas, new (32, 32, 64));
     public static BlockInfo Water { get; } = new (nameof(Water), NextId++, new (1, 1), MatterState.Liquid, new (0, 0, 255));
-    public static BlockInfo Mist { get; } = new (nameof(Mist), NextId++, new (1, 1), MatterState.Gas, new (199, 221, 255));
     public static BlockInfo Stone { get; } = new (nameof(Stone), NextId++, new (1, 1), MatterState.Solid, new (128, 128, 128));
     public static BlockInfo Sand { get; } = new (nameof(Sand), NextId++, new (1, 1), MatterState.Powder, new (194, 178, 128));
     public static BlockInfo Bedrock { get; } = new (nameof(Bedrock), NextId++, new (1, 1), MatterState.Solid, new (64, 64, 64));
@@ -22,7 +21,6 @@ public static class Blocks
         return FrozenSet.Create(
             Air,
             Water,
-            Mist,
             Stone,
             Sand,
             Bedrock,
